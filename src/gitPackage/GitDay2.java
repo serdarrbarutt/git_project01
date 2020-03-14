c41660e12d0fcd0ac49f8296fc431b9d3f776adc
Serdar Barut <serdarrbrt@gmail.com>
2020-03-14 21:07:30 +0100

    commit 6 added

--- src/gitPackage/GitDay2.java
+++ src/gitPackage/GitDay2.java
@@ -6,6 +6,7 @@ public class GitDay2 {
 
 		System.out.println("Hello world!");
 		System.out.println("Hello Git Users!");
+		System.out.println("Check if your local is ahead of origin master");
 	}
 
 }
